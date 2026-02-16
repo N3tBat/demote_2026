aaef555cabbe1faf32fef838c31f51ed81cb0250
Ernesto Zamora Barrera <ernesto.zambar05@outlook.com>
2026-02-16 17:55:47 -0600

    se agrego multiplicar

--- aritmetica.h
+++ aritmetica.h
@@ -4,6 +4,10 @@ float sumar( float a, float b) {
 }
 
 
-float resta(float a, float b){
-	return a-b;
+float restar(float a, float b){
+	return a - b;
+}
+
+float multiplicar(float a, float b){
+	return a * b;
 }
